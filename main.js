47fee7a921251ade4fc2d054e93651fcf7097bfa
Tommaso <tommaso.macco@gmail.com>
2021-11-22 18:33:00 +0100

    email list fatta

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        emailList = [
-            "Ciao",
-            "salve",
-            "tuttobene",
-            "si"
-        ]
+        emailList: [],
     },
-    methods : {
-        
+    mounted() {
+        // creo la costante self che prende il collegamento data di vue grazie al this
+        const self = this;
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(oggetto){
+                self.emailList.push(oggetto.data.response)
+            });
+        }
+        console.log(self.emailList);
     }
-
 });
